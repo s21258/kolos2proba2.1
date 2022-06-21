e1b22b7aebd408a5bb3101fdb6e71dc91a10a07d
s21258 <s21258@pjwstk.edu.pl>
2022-06-21 10:05:54 +0200

    serwisy

---
+++ Services/TeamService.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace kolos2proba2.Services
+{
+    public class TeamService
+    {
+        
+    }
+}
